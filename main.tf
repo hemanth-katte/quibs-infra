70a3bdae69a7029651aed614cc2d41d88b980bf7
Hemanth k <hemanth.k@acelucid.com>
2025-05-24 23:15:25 +0530

    chore: first commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.4.0"
+}
+
+provider "aws" {
+  profile = "quibs"
+  region  = "us-east-1"
+}
