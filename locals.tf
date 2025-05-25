99ac2a4eb8e96b65e9563f3b93454711a605ee47
hemanth-katte <hemanthss24@gmail.com>
2025-05-25 15:32:32 +0530

    chore: add locals.tf

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  default_tags = {
+    "CreatedBy"   = "terraform"
+    "Environment" = "prod"
+    "Application" = "quibs"
+    "Contact"     = "hemanth.k@acelucid.com"
+    "Managed_by"  = "acelucid_quibs"
+  }
+}
