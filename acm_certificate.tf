ade910ac8f61f330918308fa6efbfacc0aca1384
hemanth-katte <hemanthss24@gmail.com>
2025-05-26 21:17:23 +0530

    feat: acm certificate with validation

--- acm_certificate.tf
+++ acm_certificate.tf
@@ -16,3 +16,23 @@ resource "aws_acm_certificate" "quibs_cert" {
     }
   )
 }
+
+resource "aws_acm_certificate" "quibs_admin_cert" {
+  domain_name               = "quibs.com"
+  key_algorithm             = "RSA_2048"
+  subject_alternative_names = ["*.quibs.com"]
+  validation_method         = "DNS"
+
+  tags = {
+    details = "acm certificate for quibs.com with subdomains"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "quibs_cert_validation" {
+  certificate_arn         = aws_acm_certificate.quibs_admin_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
