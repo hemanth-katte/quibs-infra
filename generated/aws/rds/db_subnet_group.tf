7a022ecfd4cc52ea395a9b7db3ed861da056465f
hemanth-katte <hemanthss24@gmail.com>
2025-07-12 15:10:17 +0530

    feat: rds generated from terraformer

---
+++ generated/aws/rds/db_subnet_group.tf
@@ -0,0 +1,5 @@
+resource "aws_db_subnet_group" "tfer--default-002D-vpc-002D-025359e858376592f" {
+  description = "Created from the RDS Management Console"
+  name        = "default-vpc-025359e858376592f"
+  subnet_ids  = ["subnet-05a687e666379fe29", "subnet-075f6726de4eae9cd", "subnet-082957277bd96d4b1", "subnet-092f1c398bbf9020a", "subnet-05404b2d927691027", "subnet-0d2ea4d0ac47642a4"]
+}
---
+++ generated/aws/rds/outputs.tf
@@ -0,0 +1,11 @@
+output "aws_db_instance_tfer--quibsproductiondb_id" {
+  value = "${aws_db_instance.tfer--quibsproductiondb.id}"
+}
+
+output "aws_db_instance_tfer--quibstestdb_id" {
+  value = "${aws_db_instance.tfer--quibstestdb.id}"
+}
+
+output "aws_db_subnet_group_tfer--default-002D-vpc-002D-025359e858376592f_id" {
+  value = "${aws_db_subnet_group.tfer--default-002D-vpc-002D-025359e858376592f.id}"
+}
