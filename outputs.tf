ce09aeeb923dd83718bc635a4ecf80e2dd49f2c4
hemanth-katte <hemanthss24@gmail.com>
2025-07-28 22:53:34 +0530

    cleanup: destroy route53 records and zone

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,3 @@
-output "aws_route53_record_TXT_DMARC_quibs_id" {
-  value = aws_route53_record.TXT_DMARC_quibs.id
-}
-
-output "aws_route53_record_A_quibs_id" {
-  value = aws_route53_record.A_quibs.id
-}
-
-output "aws_route53_record_MX_quibs_id" {
-  value = aws_route53_record.MX_quibs.id
-}
-
-output "aws_route53_record_NS_quibs_id" {
-  value = aws_route53_record.NS_quibs.id
-}
-
-output "aws_route53_record_SOA_quibs_id" {
-  value = aws_route53_record.SOA_quibs.id
-}
-
-output "aws_route53_record_TXT_SPF_quibs_id" {
-  value = aws_route53_record.TXT_SPF_quibs.id
-}
-
-output "aws_route53_zone_quibs_id" {
-  value = aws_route53_zone.quibs.id
-}
-
 output "aws_instance_quib_prod_id" {
   value = aws_instance.quib_prod.id
 }
