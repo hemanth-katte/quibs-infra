b54f31847ddd0673322e454d144d19731725e6b0
hemanth-katte <hemanthss24@gmail.com>
2025-07-31 17:19:14 +0530

    cleanup: destroy quibs test ec2 instance and associated volume

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "aws_instance_quib_prod_id" {
   value = aws_instance.quib_prod.id
 }
 
-output "aws_instance_quib_test_id" {
-  value = aws_instance.quib_test.id
-}
-
 output "acm_dns_validation_records" {
   value = aws_acm_certificate.quibs_admin_cert.domain_validation_options
 }
